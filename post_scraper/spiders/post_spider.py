03958641210d21539eae58acac9b19d6b52a153b
Judd <85649796+juddisjudd@users.noreply.github.com>
2023-05-28 17:20:07 -0700

    chore/refactor: change post to embed/cleanup
    We now post as an embed with some markdown and some cleanup.

--- post_scraper/spiders/post_spider.py
+++ post_scraper/spiders/post_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-from discord_webhook import DiscordWebhook
+from discord_webhook import DiscordWebhook, DiscordEmbed
 from googletrans import Translator
 
 
@@ -10,7 +10,7 @@ class PostSpiderSpider(scrapy.Spider):
                        'RETRY_TIMES': 5,
                        'DOWNLOAD_DELAY': 1
                        }
-    webhook_url = '<replace with your discord channel webhook>'
+    webhook_url = '<insert your own discord channel webhook url>'
     search_url = 'https://gall.dcinside.com/mgallery/board/lists?id=diablo2resurrected&s_type=search_subject_memo&s_keyword=.EB.8B.A4.EC.9D.8C.20.ED.85.8C.EB.9F.AC.EC.A1.B4'
 
     def start_requests(self):
@@ -31,9 +31,33 @@ class PostSpiderSpider(scrapy.Spider):
             elif '↻' in txt or '⇆' in txt:
                 continue
             else:
+                # Apply markdown formatting
+                if ':' in txt:
+                    key, value = txt.split(':', 1)
+                    txt = f'**{key.strip()}**: {value.strip()}'
+                elif '<' in txt and '>' in txt:
+                    txt = f'**{txt}**'
+
                 translator = Translator()
                 translation = translator.translate(txt)
-                new_content.append(translation.text)
-        webhook = DiscordWebhook(url=self.webhook_url, content='\n'.join(new_content))
+
+                if translation.text.strip() != 'Mmmmmmm':  # Ignore 'Mmmmmmm' after translation
+                    new_content.append(translation.text)
+
+        # Create an embed object with red color
+        embed = DiscordEmbed(
+            title="Next Terror Zone Should Be:",
+            description='\n'.join(new_content),
+            color=16711680  # Red color
+        )
+
+        # Initialize a webhook object
+        webhook = DiscordWebhook(url=self.webhook_url)
+
+        # Add the embed object to the webhook
+        webhook.add_embed(embed)
+
+        # Execute the webhook
         response = webhook.execute()
+
         print('POST SEND...')
